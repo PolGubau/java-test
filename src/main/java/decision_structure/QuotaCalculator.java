71b2d4e727e2a80f8bdba01d8450c0ac6a2b679b
Pol Gubau <gubaupol@gmail.com>
2024-02-07 11:22:03 +0100

    First

---
+++ src/main/java/decision_structure/QuotaCalculator.java
@@ -0,0 +1,27 @@
+package decision_structure;
+
+import java.util.Scanner;
+
+public class QuotaCalculator {
+    public static void main(String[] args) {
+        int quota = 10;
+
+
+        System.out.println("Enter the number of sales:");
+        Scanner scanner = new Scanner(System.in);
+        int sales = scanner.nextInt();
+        scanner.close();
+
+        if(sales>=quota){
+            System.out.println("Congrats");
+
+        }else{
+
+            int salesShort = quota -sales;
+            System.out.println("More effort..."+
+                    "You were "+salesShort + " sales short"
+                    );
+
+        }
+    }
+}
